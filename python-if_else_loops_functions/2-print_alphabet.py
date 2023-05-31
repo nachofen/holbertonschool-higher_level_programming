108bc0cf9bc35b4975742207ed68579f690a0e1f
nachofen <zorba037@gmail.com>
2023-05-31 16:40:46 -0700

    2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123): # 97 to 123 = a to z in ascii
-    print(chr(i), end='') # end = '' prevents new line in each loop
+for i in range(97, 123):
+    print(chr(i), end='')
