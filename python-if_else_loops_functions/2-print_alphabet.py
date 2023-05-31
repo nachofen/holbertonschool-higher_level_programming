84b679e3cdb9940e311fed34f53bd96e9681401e
nachofen <zorba037@gmail.com>
2023-05-31 16:48:34 -0700

    2 fixed

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{}".format(chr(i)), end="") # end = '' prevents new line in each loop
+    print("{}".format(chr(i)), end="")  # end='' prevent new line in each loop
