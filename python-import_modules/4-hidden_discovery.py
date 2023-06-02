c99ffc586faa2269678abeae357282da0f5fd7bd
nachofen <zorba037@gmail.com>
2023-06-02 13:55:57 -0700

    4-sorted

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)  # saving all names in variable names
+    sorted_names = sorted(names)
     for name in names:
         if names[:2] != "__":
-            print("{}".format(name))
+            print("{}".format(sorted_names))
