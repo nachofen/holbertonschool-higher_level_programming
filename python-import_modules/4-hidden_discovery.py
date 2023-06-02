8a7af70971b161198994d77fe1dc0c6f40431da7
nachofen <zorba037@gmail.com>
2023-06-02 13:58:26 -0700

    4sortfix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)  # saving all names in variable names
     sorted_names = sorted(names)
-    for name in names:
+    for name in sorted_names:
         if names[:2] != "__":
             print("{}".format(sorted_names))
