101e7b032cda2e742c783b6cc89355aeb2079838
nachofen <zorba037@gmail.com>
2023-06-02 14:04:12 -0700

    4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     names = dir(hidden_4)  # saving all names in variable names
     sorted_names = sorted(names)
     for name in sorted_names:
-        if sorted_names[:2] != "__":
+        if name[:2] != "__":
             print("{}".format(name))
