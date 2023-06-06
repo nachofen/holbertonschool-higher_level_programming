690d2829ff1a0b76f5fbaa923bbab506d2448b98
nachofen <zorba037@gmail.com>
2023-06-05 21:33:39 -0700

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for sub_matrix in matrix:
         for item in sub_matrix:
             print("{:d}".format(item), end=" ")
-        print("\n", end="")
+        print()
