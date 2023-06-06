c92da499034d215725f5f3aebb0f231daaf02171
nachofen <zorba037@gmail.com>
2023-06-06 16:07:18 -0700

    8fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if sentence == "":
         first = None
-        return (0. first)
+        return (0, first)
     first = sentence[0]
     return (length, first)
