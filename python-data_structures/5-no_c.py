2dad23bb72af41fe8c03b9901d9428a8ac0e1b57
nachofen <zorba037@gmail.com>
2023-06-05 20:58:55 -0700

    5 test

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != "c"  and char != "C":
-            new_string = new_string + char
-    return(new_string)
+    if isinstance(my_string, str):
+        new_string = ""
+        for char in my_string:
+            if char != "c"  and char != "C":
+                new_string = new_string + char
+        return(new_string)
