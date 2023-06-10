cebe1631f7dbbffe6553710599e82263e3d8dbc0
nachofen <zorba037@gmail.com>
2023-06-09 19:26:48 -0700

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,28 +2,22 @@
 def roman_to_int(roman_string):
     if roman_string is None or (not isinstance(roman_string, str)):
         return 0
-    number_list = []
+    rom_val = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     result = 0
-    for number in roman_string:
-        if number == "I":
-            number_list.append(1)
-        elif number == "V":
-            number_list.append(5)
-        elif number == "X":
-            number_list.append(10)
-        elif number == "L":
-            number_list.append(50)
-        elif number == "C":
-            number_list.append(100)
-        elif number == "D":
-            number_list.append(500)
-        elif number == "M":
-            number_list.append(1000)
+    for i in range(len(roman_string)):
+        if roman_string[i] not in rom_val:
+            return 0
+        elif (i < (len(roman_string) - 1) and
+                rom_val[roman_string[i]] < rom_val[roman_string[i + 1]]):
+            result -= rom_val[roman_string[i]]
         else:
-            number_list = number_list
-    for final_number in number_list:
-        if result < final_number:
-            result = final_number - result
-        else:
-            result = result + final_number
+            result = result + rom_val[roman_string[i]]
     return result
