7e8c40959c50a7f162a2be954398aa430715398d
nachofen <zorba037@gmail.com>
2023-06-09 17:24:18 -0700

    12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return None
+    number_list = []
+    result = 0
+    for number in roman_string:
+        if number == "I":
+            number_list.append(1)
+        elif number == "V":
+            number_list.append(5)
+        elif number == "X":
+            number_list.append(10)
+        elif number == "L":
+            number_list.append(50)
+        elif number == "C":
+            number_list.append(100)
+        elif number == "D":
+            number_list.append(500)
+        elif number == "M":
+            number_list.append(1000)
+    for final_number in number_list:
+        if result < final_number:
+            result = final_number - result
+        else:
+            result = result + final_number
+    return result
