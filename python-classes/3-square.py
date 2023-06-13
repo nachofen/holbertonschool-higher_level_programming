244d5e4ad3c1713ea969d128ed217b1683ace57d
nachofen <zorba037@gmail.com>
2023-06-12 19:35:34 -0700

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the "Square with size validation"  module.
+
+This module provides a Square class with size attribute
+wich needs to be validated and add def area.
+"""
+
+
+class Square:
+    """ a class that defines a square with size as arg"""
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
