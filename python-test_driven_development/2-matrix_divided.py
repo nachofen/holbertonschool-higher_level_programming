83d32d21a1010e3c6bb3420a4cc05222f9884da2
nachofen <zorba037@gmail.com>
2023-06-14 12:03:18 -0700

    2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+shebang
+"""
+
+
+def matrix_divided(matrix, div):
+    """matrix divided by a number func"""
+    row_matrix = []
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif not isinstance(0, int) and not isinstance(0, float):
+        raise TypeError("division by zero")
+    matrix_size = len(matrix[0])
+    for col in matrix:
+        if matrix_size != len(col):
+            raise TypeError("Each row of the matrix must have the same size")
+        for row in col:
+            if not isinstance(row, (int, float)):
+                raise TypeError(error)
+        if matrix_size != len(col):
+            raise TypeError("Each row of the matrix must have the same size")
+    for col in matrix:
+        new_matrix = []
+        for row in col:
+            row_matrix.append(round(row / div, 2))
+        new_matrix.append(row_matrix)
+    return (new_matrix)
