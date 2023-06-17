4155c907761a42a46ae059d6e4b71bf57cfed3c0
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-17 08:36:46 -0700

    5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+shebang
+"""
+
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
