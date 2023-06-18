e210e38c12f5f21205beafd30f28fcad8952cf84
nachofen <zorba037@gmail.com>
2023-06-18 14:37:25 -0700

    3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle with width and height validation"  module.
+"""
+
+
+class Rectangle:
+    """ a class that defines a rectangle with width and height as args"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        area = self.width * self.height
+        return(area)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            peri = 0
+            return peri
+        peri = (self.width * 2) + (self.height * 2)
+        return (peri)
+
+    def __str__(self):
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += "#" * self.width
+            rectangle_str += "\n"
+        return rectangle_str[:-1]
