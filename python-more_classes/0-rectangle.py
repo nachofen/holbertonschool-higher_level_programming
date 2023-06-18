c40e53df2975db5c27eb86e57dbd08476a7985cb
nachofen <zorba037@gmail.com>
2023-06-18 09:37:12 -0700

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the "Square with size validation"  module.
+This module provides a Square class with size attribute
+and needs to be validated.
+"""
+
+
+class Rectangle:
+    """Empty Class Rectangle"""
+    pass
