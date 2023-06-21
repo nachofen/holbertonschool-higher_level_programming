e51dc750f6026ce2b74a542e14e841c0370b5186
nachofen <zorba037@gmail.com>
2023-06-21 07:29:03 -0700

    1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define mylist class."""
+
+class MyList(list):
+    """Define a MyList."""
+
+    def print_sorted(self):
+        """print a list sorted in ascending order"""
+        print(sorted(self))
