087e948a1c0ea6c9445a826ff3fafcae518936ae
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-22 11:52:16 -0700

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 0- Read file."""
+
+
+def read_file(filename=""):
+    """reads a text file using UTF8 and prints it to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        text = file.read()
+        print(text)
