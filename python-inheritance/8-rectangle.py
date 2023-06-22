44395b4ee320828db10a69176dd20e98e97baeb8
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-22 07:53:04 -0700

    8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""task 8"""
+
+
+class BaseGeometry:
+    """Basegeometry class"""
+
+    def area(self):
+        """def area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """def integer validator"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        """def init"""
+        self.integer_validator("width", width)
+        __width = width
+        self.integer_validator("height", height)
+        __height = height
