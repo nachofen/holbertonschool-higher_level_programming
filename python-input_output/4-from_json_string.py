a07842eb8c1429cab967a7f7c744a47fcdfac897
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-22 12:58:47 -0700

    4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,6 +4,6 @@ import json
 
 
 def from_json_string(my_str):
-    """returns the JSON representation of an object(string)"""
+    """returns an object represented by a JSON(string)"""
     my_obj = json.loads(my_str)
     return (my_obj)
