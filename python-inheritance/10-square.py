52549ec5a53ae5202d498e50fba08d7e4bb4f22e
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-22 08:28:46 -0700

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""task 10"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square using Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Arg:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, size, size)
+        """this line could be super() too but witout self as arg"""
+        self.__size = size
+
+    def area(self):
+        """returns the square area"""
+        return (self.__size * self.__size)
