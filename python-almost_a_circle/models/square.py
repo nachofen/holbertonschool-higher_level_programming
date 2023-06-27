e4fb5051b1806651b5dbc82ce2c23aa936dacbea
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-27 07:37:21 -0700

    11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,3 +23,13 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.width)
+
+    @property
+    def size(self):
+        """get and set for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
