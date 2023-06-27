3f2137c368fc1eb2ef7fede6a5c5c7725391e027
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-27 08:36:31 -0700

    15fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns JSON string rep of list_dictionaries"""
-        if list_dictionaries and len(list_dictionaries) > 0:
-            return json.dumps(list_dictionaries)
-        else:
+        if list_dictionaries is None or len(list_dictionaries) > 0:
             return "[]"
+        else:
+            return json.dumps(list_dictionaries)
