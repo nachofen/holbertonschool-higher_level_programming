8c998dc9b27948f38520d6c668f2945d83ea6f8d
nachofen <124267664+nachofen@users.noreply.github.com>
2023-06-26 20:23:56 -0700

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Square """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """new square constructor
+        Args:
+            size (int): The size of the new square.
+            x (int): The x coordinate of the new square.
+            y (int): The y coordinate of the new square.
+            id (int): The identity of the new square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """overriding __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.width)
