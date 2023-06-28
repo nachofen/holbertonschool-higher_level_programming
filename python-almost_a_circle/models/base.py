12bdec04d904dff4a91797b7cbd9051c93dbf779
nachofen <zorba037@gmail.com>
2023-06-28 12:34:44 -0700

    19 to fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Base class"""
 import json
+import os
 
 
 class Base:
     """represents base model"""
     __nb_objects = 0
-
     def __init__(self, id=None):
         """constructor
         arg: id(int)
@@ -16,7 +16,6 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string rep of list_dictionaries"""
@@ -24,7 +23,6 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
@@ -33,7 +31,6 @@ class Base:
             return my_list
         else:
             return json.loads(json_string)
-
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
@@ -51,5 +48,19 @@ class Base:
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
         dummy_square = cls(**dictionary)
-        dummy_square.update(**dictionary)
+        dummy_square.__dict__.update(**dictionary)
         return dummy_square
+    
+    @classmethod
+    def load_from_file(cls):
+        """that returns a list of instances"""
+        file_name = cls.__name__ + ".json"
+        if os.path.exists(file_name):
+            ins_list = []
+            with open(file_name) as file:
+                my_dict = cls.from_json_string(file.read())
+                for dicts in my_dict:
+                    ins_list.append(cls.create(**dicts))
+            return ins_list
+        else:
+            return []
